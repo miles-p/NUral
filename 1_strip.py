d30808c3724d115d90ff5a5d64c1bcfa8bfa55ff
Miles Punch <miles.anyprintz@gmail.com>
2026-06-03 14:31:31 +1000

    fix a thing in 1_strip.py

--- 1_strip.py
+++ 1_strip.py
@@ -15,7 +15,7 @@ def iter_records(path):
 
 def iter_imu_rows(path):
     for rec in iter_records(path):
-        if rec.get("type") != "message.input.Sensors":
+        if rec.get("type") != "message.platform.RawSensors":
             continue
         data = rec.get("data", {})
         accel = data.get("accelerometer", {}) or {}
